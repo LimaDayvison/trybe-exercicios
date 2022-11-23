988bd2d0d8660608756a237d76bbebaae3ec3f1c
Dayvison Lima <limaare@outlook.com>
2022-11-23 19:23:00 -0300

    transformando tudo em comentário

--- fundamentos/secao-4-introducao-a-javascript/script.js
+++ fundamentos/secao-4-introducao-a-javascript/script.js
@@ -125,33 +125,33 @@
 // }
 
 
-let aliquotINSS;
-let aliquotIR;
+// let aliquotINSS;
+// let aliquotIR;
 
-const grossSalary = 3500.00;
+// const grossSalary = 3500.00;
 
-if (grossSalary <= 1556.94) {
-  aliquotINSS = grossSalary * 0.08;
-} else if (grossSalary <= 2594.92) {
-  aliquotINSS = grossSalary * 0.09;
-} else if (grossSalary <= 5189.82) {
-  aliquotINSS = grossSalary * 0.11;
-} else {
-  aliquotINSS = 570.88;
-}
+// if (grossSalary <= 1556.94) {
+//   aliquotINSS = grossSalary * 0.08;
+// } else if (grossSalary <= 2594.92) {
+//   aliquotINSS = grossSalary * 0.09;
+// } else if (grossSalary <= 5189.82) {
+//   aliquotINSS = grossSalary * 0.11;
+// } else {
+//   aliquotINSS = 570.88;
+// }
 
-const baseSalary = grossSalary - aliquotINSS;
+// const baseSalary = grossSalary - aliquotINSS;
 
-if (baseSalary <= 1903.98) {
-  aliquotIR = 0;
-} else if (baseSalary <= 2826.65) {
-  aliquotIR = (baseSalary * 0.075) - 142.80;
-} else if (baseSalary <= 3751.05) {
-  aliquotIR = (baseSalary * 0.15) - 354.80;
-} else if (baseSalary <= 4664.68) {
-  aliquotIR = (baseSalary * 0.225) - 636.13;
-} else {
-  aliquotIR = (baseSalary * 0.275) - 869.36;
-};
+// if (baseSalary <= 1903.98) {
+//   aliquotIR = 0;
+// } else if (baseSalary <= 2826.65) {
+//   aliquotIR = (baseSalary * 0.075) - 142.80;
+// } else if (baseSalary <= 3751.05) {
+//   aliquotIR = (baseSalary * 0.15) - 354.80;
+// } else if (baseSalary <= 4664.68) {
+//   aliquotIR = (baseSalary * 0.225) - 636.13;
+// } else {
+//   aliquotIR = (baseSalary * 0.275) - 869.36;
+// };
 
-console.log("Salário: " + (baseSalary - aliquotIR));
+// console.log("Salário: " + (baseSalary - aliquotIR));
