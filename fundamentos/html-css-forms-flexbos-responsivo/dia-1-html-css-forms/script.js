b7ac9b9cc536885559fba6d5adaa81ee398b04cd
Dayvison Lima <limaare@outlook.com>
2023-01-10 17:38:23 -0300

    resolvidos exercicios do dia

---
+++ fundamentos/html-css-forms-flexbos-responsivo/dia-1-html-css-forms/script.js
@@ -0,0 +1,28 @@
+const handleSubmit = (event) => {
+    event.preventDefault();
+};
+
+
+
+const clearFields = () => {
+    const formElements = document.querySelectorAll('input')
+    const textArea = document.querySelector('textarea');
+
+    for(let index = 0; index < formElements.length; index += 1){
+        const userInput = formElements[index];
+        if(formElements[index].type === 'radio' || formElements[index].type === 'checkbox'){
+            userInput.checked = false;
+        } else {
+            userInput.value = '';
+        }
+    }
+
+    textArea.value = '';
+}
+
+window.onload = () => {
+    const submitBtn = document.querySelector('#submit-btn');
+    submitBtn.addEventListener('click', handleSubmit);
+    const clearBtn = document.querySelector('#clear-btn');
+    clearBtn.addEventListener('click', clearFields)
+};
